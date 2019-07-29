c2b81ec7721a8f31a10ca62bba2587083987692d
icannotdothecode <vishals0705@gmail.com>
2019-07-28 22:59:56 -0700

    ok its done

--- testingequality.java
+++ testingequality.java
@@ -1,11 +1,17 @@
 ///testing if three integers are equal
-public class aretheyequal {
+public class testingequality {
     public static void main(String[] args){
         if (args.length == 0) {
-            throw new IllegalArgumentException("Numbers are required! eg: java L00ps.java 1 2")
+            throw new IllegalArgumentException("Numbers are required! eg: java testingequality.java 1 2 3");
         }
         int x = Integer.parseInt(args[0]);
         int y = Integer.parseInt(args[1]);
-
+        int z = Integer.parseInt(args[2]);
+        if (x == y && x == z){
+            System.out.println("All integers are equal.");
+        }
+        else {
+            System.out.println("All integers are not equal.");
+        }
     }
 }
